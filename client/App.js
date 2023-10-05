04e7c7e8c3d37aafd9931f2c7c0613375a82fe84
tula83 <131385747+tula83@users.noreply.github.com>
2023-10-05 20:15:51 +0530

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Content from './components/Content';
+
+import Products from './components/Products';
+function App() {
+  return (
+    <div className="App">
+      <Content></Content>
+    <Products/>
+     
+    </div>
+  );
+}
+
+export default App;
